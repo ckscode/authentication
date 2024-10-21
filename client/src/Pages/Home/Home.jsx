6fed1cd41c5de14f1c39f21c63f4614dfb5fba30
ckscode <cksiddhaarthan@gmail.com>
2024-10-21 21:36:16 +0530

    navigate

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -7,7 +8,8 @@ const Home = () => {
         <div className='d-flex justify-content-center align-items-center'>
           <div className='text-center my-5'>
           <h1>Ultimate Authentication Boilerplate</h1>
-          <h5>MERN Stack Authentication with Google and Facebook</h5>
+          <h5>MERN Stack Authentication with Google and Facebook with Razorpay</h5>
+          <Link to="/order" className="btn btn-primary">Razorpay</Link>
           </div>
         </div>
     );
